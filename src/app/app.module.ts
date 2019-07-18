f2bd44b91725debe100228a35fab86d5478f9962
Dan Cornilov <dancornilov@gmail.com>
2019-07-18 20:28:31 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CameraCaptureComponent } from './camera-capture/camera-capture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraCaptureComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/camera-capture/camera-capture.component.ts
@@ -0,0 +1,109 @@
+import { Component, ElementRef, EventEmitter, OnDestroy, Output, ViewChild } from '@angular/core';
+
+enum CameraStatuses {
+  disabled = 'disabled',
+  enabled = 'enabled',
+  taken = 'taken'
+}
+
+@Component({
+  selector: 'app-camera-capture',
+  templateUrl: './camera-capture.component.html',
+  styleUrls: ['./camera-capture.component.scss']
+})
+export class CameraCaptureComponent implements OnDestroy {
+  @ViewChild('video') public video: ElementRef;
+  @ViewChild('canvas') public canvas: ElementRef;
+  @Output() public capture: EventEmitter<File> = new EventEmitter<File>();
+
+  public captured: Blob;
+  public enabled: boolean;
+  public status: CameraStatuses;
+  public statuses = CameraStatuses;
+  private stream: MediaStream;
+
+  constructor() {
+    this.status = CameraStatuses.disabled;
+  }
+
+  public ngOnDestroy(): void {
+    if (this.stream) {
+      this.stream.getVideoTracks()[0].stop();
+    }
+  }
+
+  public getButtonName(): string {
+    switch (this.status) {
+      case CameraStatuses.disabled:
+        return 'Start';
+      case CameraStatuses.enabled:
+        return 'Take Photo';
+      case CameraStatuses.taken:
+        return 'Retake Photo';
+      default:
+        break;
+    }
+  }
+
+  public executeButtonAction(): void {
+    switch (this.status) {
+      case CameraStatuses.disabled:
+        this.enableCamera();
+        break;
+      case CameraStatuses.enabled:
+        this.makeCapture();
+        break;
+      case CameraStatuses.taken:
+        this.resetCanvas();
+        break;
+      default:
+        break;
+    }
+  }
+
+  private enableCamera(): void {
+    this.status = CameraStatuses.enabled;
+    this.enabled = true;
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        this.stream = stream;
+
+        try {
+          this.video.nativeElement.srcObject = stream;
+        } catch (error) {
+          this.video.nativeElement.src = URL.createObjectURL(stream);
+        }
+
+        this.video.nativeElement.play();
+      }).catch(() => {
+        console.log('Please give access to camera');
+        this.status = CameraStatuses.disabled;
+      });
+    }
+  }
+
+  private makeCapture(): void {
+    this.status = CameraStatuses.taken;
+    this.canvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement, 0, 0, 640, 480);
+
+    this.captured = this.canvas.nativeElement.toDataURL('image/png');
+    this.dataURItoBlob(this.canvas.nativeElement.toDataURL('image/png'));
+  }
+
+  private resetCanvas() {
+    this.status = CameraStatuses.enabled;
+    this.captured = undefined;
+    const context = this.canvas.nativeElement.getContext('2d');
+
+    context.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.capture.emit();
+  }
+
+  private dataURItoBlob(url): void {
+    fetch(url).then(res => res.blob()).then(blob => {
+      const file = new File([blob], `Selfie_${ Date.now() }.png`, { type: 'image/png' });
+
+      this.capture.emit(file);
+    });
+  }
+}
